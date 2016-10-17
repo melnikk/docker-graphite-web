18384a3cfcd3f32ca9bb42daa490c1bd12d00a8d
AlexAkulov <alexakulov86@gmail.com>
2016-10-17 14:47:09 +0500

    Add many graphite-web settings and fix nginx

--- config/local_settings.py
+++ config/local_settings.py
@@ -17,3 +17,33 @@ if os.getenv("SECRET_KEY"):
     SECRET_KEY = os.getenv("SECRET_KEY")
 else:
     SECRET_KEY = "secret_key"
+
+if os.getenv("CARBONLINK_TIMEOUT"):
+    CARBONLINK_TIMEOUT = os.getenv("CARBONLINK_TIMEOUT")
+
+if os.getenv("CARBONLINK_TIMEOUT"):
+    CARBONLINK_TIMEOUT = os.getenv("CARBONLINK_RETRY_DELAY")
+
+if os.getenv("CARBONLINK_QUERY_BULK"):
+    CARBONLINK_QUERY_BULK = os.getenv("CARBONLINK_QUERY_BULK")
+
+if os.getenv("REMOTE_STORE_FETCH_TIMEOUT"):
+    REMOTE_STORE_FETCH_TIMEOUT = os.getenv("REMOTE_STORE_FETCH_TIMEOUT")
+
+if os.getenv("REMOTE_STORE_FIND_TIMEOUT"):
+    REMOTE_STORE_FIND_TIMEOUT = os.getenv("REMOTE_STORE_FIND_TIMEOUT")
+
+if os.getenv("REMOTE_STORE_RETRY_DELAY"):
+    REMOTE_STORE_RETRY_DELAY = os.getenv("REMOTE_STORE_RETRY_DELAY")
+
+if os.getenv("REMOTE_FIND_CACHE_DURATION"):
+    REMOTE_FIND_CACHE_DURATION = os.getenv("REMOTE_FIND_CACHE_DURATION")
+
+if os.getenv("REMOTE_PREFETCH_DATA"):
+    REMOTE_PREFETCH_DATA = os.getenv("REMOTE_PREFETCH_DATA")
+
+if os.getenv("BAN_PREFIX"):
+    BAN_PREFIX = os.getenv("BAN_PREFIX")
+
+if os.getenv("TZ"):
+    TIME_ZONE = os.getenv("TZ")
